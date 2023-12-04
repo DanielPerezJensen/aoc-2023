6059baf1088f41f19797d66f31dc0d3377831d3d
Daniel Perez <danielperezjensen@gmail.com>
2023-12-04 08:30:42 +0100

    fix: fixed division by zero error

--- day_4/main.py
+++ day_4/main.py
@@ -39,19 +39,11 @@ def part_1(data):
 
     solution = 0
 
-    for i, game in enumerate(games):
-        card_number, numbers, winning_numbers = game
-        numbers = set(numbers)
-        winning_numbers = set(winning_numbers)
+    winning_numbers = get_winning_numbers(games)
+    card_counts = np.ones(winning_numbers.shape, dtype=int)
 
-        winning_amount = len(numbers.intersection(winning_numbers))
-
-        if winning_amount == 0:
-            points = 0
-        else:
-            points = 2 ** (winning_amount - 1)
-
-        solution += points
+    for i, winning_amount in enumerate(winning_numbers):
+        solution += 2 ** (winning_amount - 1) if winning_amount > 0 else 0
 
     print(solution)
     submit(solution, part="a", day=4, year=2023)
@@ -68,7 +60,8 @@ def part_2(data):
         winning_amount = winning_numbers[i]
         added_cards = winning_amount * card_count
 
-        card_counts[i + 1 : winning_amount + i + 1] += added_cards // winning_amount
+        if winning_amount > 0:
+            card_counts[i + 1 : winning_amount + i + 1] += added_cards // winning_amount
 
     solution = int(np.sum(card_counts))
     print(solution)
