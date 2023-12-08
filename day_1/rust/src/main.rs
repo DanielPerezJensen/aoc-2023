ac17f10437dd559b8d81a96aed3258aefe58b161
Daniel Perez <danielperezjensen@gmail.com>
2023-12-08 09:30:10 +0100

    feat: added day 1, part 1 rust implementation

--- day_1/rust/src/main.rs
+++ day_1/rust/src/main.rs
@@ -1,3 +1,45 @@
+const _TEST1: &'static str = include_str!("test1.txt");
+const _TEST2: &'static str = include_str!("test2.txt");
+const _DATA: &'static str = include_str!("data.txt");
+
+fn part1(input: &str) -> u32 {
+    let mut solution: u32 = 0;
+
+    for line in input.lines() {
+        let mut first: u32 = 0;
+        let mut second: u32 = 0;
+
+        for ch in line.chars() {
+            if ch.is_ascii_digit() {
+                first = ch.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        for ch in line.chars().rev() {
+            if ch.is_ascii_digit() {
+                second = ch.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        solution = solution + first * 10 + second
+        // let mut lines = input.lines();
+    }
+    return solution;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = part1(_TEST1);
+    println!("Part 1: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(part1(_TEST1), 142)
+    }
 }
