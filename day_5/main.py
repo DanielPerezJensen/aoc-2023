f15a93b0f308896812dbed81a140fd7a0de77eca
Daniel Perez <danielperezjensen@gmail.com>
2023-12-09 15:53:07 +0100

    feat: too easy for day 9

--- day_5/main.py
+++ day_5/main.py
@@ -2,26 +2,142 @@ import re
 
 import numpy as np
 from aocd import get_data, submit
+from tqdm import tqdm
 
 
 def parse_data(data):
-    pass
+    maps = []
+
+    data_split = data.split("\n\n")
+    seeds = data_split[0]
+    seeds = [int(x) for x in re.findall(r"\d+", seeds)]
+
+    for line in data_split[1:]:
+        map_lines = []
+        map_name, rest = line.split(" map:")
+        for line_2 in rest.split("\n")[1:]:
+            map_lines.append(tuple([int(x) for x in re.findall(r"\d+", line_2)]))
+        maps.append(map_lines)
+
+    return seeds, maps
+
+
+def perform_mapping_part_1(seed, maps):
+    val = seed
+
+    for mapping in maps:
+        mapped = False
+        for dst, src, size in mapping:
+            if src <= val < src + size:
+                off = val - src
+                new_val = dst + off
+                mapped = True
+
+        if not mapped:
+            new_val = val
+
+        val = new_val
+
+    return val
 
 
 def part_1(data):
-    pass
-    # submit(solution, part="a", day=4, year=2023)
+    seeds, maps = parse_data(data)
+
+    seed_to_destination = [perform_mapping_part_1(seed, maps) for seed in seeds]
+
+    solution = min(seed_to_destination)
+
+    submit(solution, part="a", day=5, year=2023)
+
+
+def perform_mapping_part_2(R, mapping):
+    A = []
+
+    for dest, src, size in mapping:
+        src_end = src + size
+        NR = []
+
+        while R:
+            start, end = R.pop()
+            before = (start, min(end, src))
+            inter = (max(start, src), min(end, src_end))
+            after = (max(start, src_end), end)
+
+            if before[1] > before[0]:
+                NR.append(before)
+            if inter[1] > inter[0]:
+                A.append((inter[0] - src + dest, inter[1] - src + dest))
+            if after[1] > after[0]:
+                NR.append(after)
+
+        R = NR
+
+    return A + R
 
 
 def part_2(data):
-    pass
-    # submit(solution, part="b", day=4, year=2023)
+    seeds, maps = parse_data(data)
+    print("*" * 25)
+    for map in maps:
+        print(map)
+
+    solution = 99999999
+
+    for seed_range in list(zip(seeds[::2], seeds[1::2])):
+        # [x, y)
+        seed_range = [(seed_range[0], seed_range[0] + seed_range[1])]
+        for map in maps:
+            seed_range = perform_mapping_part_2(seed_range, map)
+
+        solution = min(solution, min(seed_range)[0])
+
+    print(solution)
+    submit(solution, part="b", day=5, year=2023)
 
 
-data = """"""
+data = """seeds: 79 14 55 13
 
+seed-to-soil map:
+50 98 2
+52 50 48
 
-data = get_data(day=5, year=2023)
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
 
-part_1(data)
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+groups = data.split("\n\n")
+
+for g in groups[1:]:
+    step_mapping = [tuple(map(int, l.split())) for l in g.splitlines()[1:]]
+
+    print(step_mapping)
+
+# data = get_data(day=5, year=2023)
+
+# part_1(data)
 part_2(data)
--- day_9/main.py
+++ day_9/main.py
@@ -5,23 +5,83 @@ from aocd import get_data, submit
 
 
 def parse_data(data):
-    pass
+    sequences = []
+    for line in data.splitlines():
+        sequences.append([int(x) for x in re.findall(r"-?\d+", line)])
+    return sequences
+
+
+def parse_sequence(line, part2=False):
+    steps = []
+    for i in range(len(line))[:-1]:
+        digit = line[i]
+        step = int(line[i + 1]) - int(digit)
+        steps.append(step)
+
+    return steps
+
+
+def create_pyramid(sequence, part2=False):
+    pyramid = [sequence]
+    while not all([x == 0 for x in sequence]):
+        sequence = parse_sequence(sequence)
+        pyramid.append(sequence)
+
+    return pyramid
+
+
+def backfill(pyramid):
+    pyramid.reverse()
+
+    for i in range(len(pyramid) - 1):
+        current_row = pyramid[i]
+        next_row = pyramid[i + 1]
+        final_value = current_row[-1]
+        next_row.append(final_value + next_row[-1])
+
+    return pyramid
 
 
 def part_1(data):
-    pass
-    # submit(solution, part="a", day=4, year=2023)
+    sequences = parse_data(data)
+
+    solution = 0
+
+    for sequence in sequences:
+        pyramid = create_pyramid(sequence)
+
+        new_sequences = backfill(pyramid)
+
+        solution += new_sequences[-1][-1]
+
+    print(solution)
+    submit(solution, part="a", day=9, year=2023)
 
 
 def part_2(data):
-    pass
-    # submit(solution, part="b", day=4, year=2023)
+    sequences = parse_data(data)
+
+    solution = 0
+
+    for sequence in sequences:
+        sequence.reverse()
+
+        pyramid = create_pyramid(sequence)
+
+        new_sequences = backfill(pyramid)
+
+        solution += new_sequences[-1][-1]
+
+    print(solution)
+    submit(solution, part="b", day=9, year=2023)
 
 
-data = """"""
+data = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
 
 
-# data = get_data(day=9, year=2023)
+data = get_data(day=9, year=2023)
 
 part_1(data)
 part_2(data)
