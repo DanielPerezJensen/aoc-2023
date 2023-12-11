0dcd944ae6e3ad122ae0a375ed78af708a17b883
Daniel Jensen <daniel.jensen@asml.com>
2023-12-11 09:58:54 +0100

    feat: finished part a of day 11

--- day_11/main.py
+++ day_11/main.py
@@ -1,16 +1,49 @@
 import re
+from itertools import combinations
 
 import numpy as np
 from aocd import get_data, submit
 
 
 def parse_data(data):
-    pass
+    matrix = [[l for l in line] for line in data.splitlines()]
+
+    return matrix
+
+
+def manhattan_distance(coord1, coord2):
+    return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
 
 
 def part_1(data):
-    pass
-    # submit(solution, part="a", day=11, year=2023)
+    matrix = parse_data(data)
+
+    empty_rows = [y for y, row in enumerate(matrix) if all(char == "." for char in row)]
+    empty_cols = [x for x, cols in enumerate(zip(*matrix)) if all(char == "." for char in cols)]
+
+    width = len(matrix[0])
+    for row in reversed(empty_rows):
+        matrix.insert(row, ["."] * width)
+
+    height = len(matrix)
+    for col in reversed(empty_cols):
+        for row in range(height):
+            matrix[row] = matrix[row][:col] + ["."] + matrix[row][col:]
+
+    coords = []
+
+    for y, row in enumerate(matrix):
+        for x, char in enumerate(row):
+            if char == "#":
+                coords.append((x, y))
+
+    shortest_paths = [
+        manhattan_distance(coord1, coord2) for coord1, coord2 in combinations(coords, 2)
+    ]
+
+    solution = sum(shortest_paths)
+    print(solution)
+    submit(solution, part="a", day=11, year=2023)
 
 
 def part_2(data):
@@ -18,10 +51,18 @@ def part_2(data):
     # submit(solution, part="b", day=11, year=2023)
 
 
-data = """"""
+data = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
 
-
-# data = get_data(day=11, year=2023)
+data = get_data(day=11, year=2023)
 
 part_1(data)
-part_2(data)
+# part_2(data)
