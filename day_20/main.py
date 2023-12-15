3e416ab815ac2ef4ad5c8f7d5482a5e2aaad423d
Daniel Jensen <daniel.jensen@asml.com>
2023-12-15 09:26:47 +0100

    build: added base files for rest of days

---
+++ day_20/main.py
@@ -0,0 +1,27 @@
+import re
+
+import numpy as np
+from aocd import get_data, submit
+
+
+def parse_data(data):
+    pass
+
+
+def part_1(data):
+    pass
+    # submit(solution, part="a", day=20, year=2023)
+
+
+def part_2(data):
+    pass
+    # submit(solution, part="b", day=20, year=2023)
+
+
+data = """"""
+
+
+# data = get_data(day=20, year=2023)
+
+part_1(data)
+part_2(data)
---
+++ day_23/main.py
@@ -0,0 +1,27 @@
+import re
+
+import numpy as np
+from aocd import get_data, submit
+
+
+def parse_data(data):
+    pass
+
+
+def part_1(data):
+    pass
+    # submit(solution, part="a", day=23, year=2023)
+
+
+def part_2(data):
+    pass
+    # submit(solution, part="b", day=23, year=2023)
+
+
+data = """"""
+
+
+# data = get_data(day=23, year=2023)
+
+part_1(data)
+part_2(data)
