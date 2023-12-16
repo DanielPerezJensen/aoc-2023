99392dd97de746c2bb9eeadff86d976c2d22bf11
Daniel Perez <danielperezjensen@gmail.com>
2023-12-16 11:12:18 +0100

    feat: finished part 2 of day 14

--- day_14/main.py
+++ day_14/main.py
@@ -35,9 +35,8 @@ def part_1(data):
                 solution += i + 1
 
     print(solution)
-    # print(new_col)
 
-    # submit(solution, part="a", day=14, year=2023)
+    submit(solution, part="a", day=14, year=2023)
 
 
 char_map = {"#": 0, ".": 2, "O": 1}
@@ -61,6 +60,7 @@ def chain(*iterables):
     for it in iterables:
         for each in it:
             yield each
+        yield 0
 
 
 @cache
@@ -73,37 +73,46 @@ def roll_direction(boulder_map, dir="n"):
         for i, col in enumerate(columns):
             col_items = split_tuple(col, 0)
 
-            print(col_items)
-
-            sorted_items = [sorted(item) + [0] for item in col_items]
-            sorted_items_flattened = list(chain(*sorted_items))
-            # print(sorted_items_flattened)
-            new_boulder_map.append(tuple(sorted_items_flattened))
+            sorted_items = [sorted(item) for item in col_items]
+            sorted_items_flattened = tuple(chain(*sorted_items))[:-1]
+            new_boulder_map.append(sorted_items_flattened)
 
         new_boulder_map = tuple(tuple(c) for c in zip(*new_boulder_map))
+
+    elif dir == "w":
+        for row in boulder_map:
+            row_items = split_tuple(row, 0)
+            sorted_items = [sorted(item) for item in row_items]
+            sorted_items_flattened = tuple(chain(*sorted_items))[:-1]
+            new_boulder_map.append(sorted_items_flattened)
 
     elif dir == "s":
         columns = list(list(c) for c in zip(*boulder_map))
 
         for i, col in enumerate(columns):
-            col_str = "".join(map(str, col))
-            col_items = col_str.split("0")
+            col_items = split_tuple(col, 0)
             sorted_items = [sorted(item, reverse=True) for item in col_items]
-            sorted_str = "0".join("".join(s) for s in sorted_items)
-            new_boulder_map.append(tuple(int(s) for s in sorted_str))
+            sorted_items_flattened = tuple(chain(*sorted_items))[:-1]
+            new_boulder_map.append(sorted_items_flattened)
 
         new_boulder_map = tuple(tuple(c) for c in zip(*new_boulder_map))
 
-    return new_boulder_map
+    elif dir == "e":
+        for row in boulder_map:
+            row_items = split_tuple(row, 0)
+            sorted_items = [sorted(item, reverse=True) for item in row_items]
+            sorted_items_flattened = tuple(chain(*sorted_items))[:-1]
+            new_boulder_map.append(sorted_items_flattened)
 
-
-seen = dict()
+    return tuple(new_boulder_map)
 
 
 @cache
 def run_loop(boulder_map):
     boulder_map = roll_direction(boulder_map, dir="n")
+    boulder_map = roll_direction(boulder_map, dir="w")
     boulder_map = roll_direction(boulder_map, dir="s")
+    boulder_map = roll_direction(boulder_map, dir="e")
 
     return boulder_map
 
@@ -114,122 +123,23 @@ def part_2(data):
     boulder_map = parse_data(data)
     boulder_map = tuple(tuple(s) for s in boulder_map)
 
-    # for _ in tqdm(range(1_000_000_000)):
-    #     new_boulder_map = run_loop(boulder_map)
+    seen = {}
 
-    for col in boulder_map:
-        print(" ".join(list(reverse_char_map[c] for c in col)))
+    iterations = 1_000
 
-    new_boulder_map = roll_direction(boulder_map, dir="n")
+    for i in range(iterations):
+        boulder_map = run_loop(boulder_map)
 
-    print("*" * 25)
+    solution = 0
 
-    for col in new_boulder_map:
-        print(" ".join(list(reverse_char_map[c] for c in col)))
+    for col in list(zip(*boulder_map)):
+        for i, row in enumerate(col[::-1]):
+            if row == 1:
+                solution += i + 1
 
-    # submit(solution, part="b", day=14, year=2023)
+    print(solution)
+    submit(solution, part="b", day=14, year=2023)
 
-
-data = """.#.OO.O.#OO........O....O..OO....OOO...#....#..........#.O.OO.....O#.#..#..#O##.O..#.#.O#.O.OO.....#
-.O##.#..O..##..#..#.#..O#....O##..#.....##.......O...O#O..O.......#..#..O#O...#O..OO.#.....O.##.#.##
-#.O...O.#.O....#.O..#OO...O...#.#.##...........#....#O.....##..#.O.#.............O...O..O.O...##...#
-.#.#.....O..#...#...O.####OO....#.O...O...O...O.....OO#.O.##O.............O...#O.#OO#......O........
-OO.O.....O....O..OO..O#..OO.OOO..OO...........O..##.O...O.#.O...##.O##....O.OO.#.O.....O..#.O...O##O
-.#..##....#.O#...........#..OO...O......O.......#O..O..#...O......#.#....#.OO#..#O...O...#.....O..O#
-##..OO.##.#........OOO##...OO.OOO#O...OOO.O.O...#...#.##..O##...#.#O#....#........#.O....#O.#O.O..O.
-...O.O.O#...O.....O..OO...#O..#.#...O......#.......#.O...O.O#....O......#.OO.O..OOO.OO.O.O.###O...OO
-...#O.....#O..##..#O.#...O.#...OO#O#.#..#.#....O..O.O.#...O.O...O.#..##O...O.#.OO.#O...OO.O.#O.OO#..
-#.....#.#.O..#....O....##.O#......OO#....O#.#O.#..#O..#OO..#O..O...##O..#.....O##...O#.#..OO....O..#
-.#OOO...#O...#...##...#O....#O..........O.#.O.O#..#..#OO.#............#....O#.#O....#....OO..#...O.O
-#...OO...O...O..O.#.........O#O##..O........O.#..#O#O.##....#...#O.....OOO.#......#....O.O..#.OO.#..
-.OO.....#..##...#.O.#O.#...##..#...#O.#...O#........O.O....#.##....O...#..#...#.O....OO..OO....O..#.
-#..#.....OO....#O.#.O...O.OOO..O..O.#.O...OOOO..O.O...#..OOO...#......#.###.O.O....O.......O....O.#.
-.#O.#....O....O#.OO..O.OOO...........O.##........O#O..#...O.#...O....O..O.##.#O#.#OO.O...O#...#..#O.
-.OO.#O.#..O.O.OO...O.O....OOOO#.O.O.#.#.O.#.#....OO.#....#..#.....##...O....OO.OO...#.O.O...O.....OO
-...###..O......#O.....#.....#...O.......O.#O.O..#..........OO..O.#..O...#..#.O.O....#..#.#...O.OOO..
-........O..O.......O.......O#.O.O...#.#....#........#.#....OO....#.#........###.#...O...#OO.O#.#....
-.OO#...........#......#.......#....O......#......##.O......OO.O#...O...O....O######O#..O..O.....OO..
-.O.........OOO#O.#O...#.O....OOO##.......O.#..O..#..#O........O.OO..O..#...#O#..##..O#.###....OO....
-.#.#.#O..#..#OO...O......O....OO..O......O......#..O..#...O.#..O...#....OOO....OO.OO#O....O.#.......
-..OO..O.O..O#..#......OO.........#.O..O.O..#......#OO.#..O.O.#O#..O.#O...O.O..O.#......O...OOO.OO..#
-O.O##....O.O....#.....#...OO..O..#.##..O..O###....#...O.#.O#O.OO.OO#.#...#.....#.#....O...#OOO..O#..
-#.#.O.#O.O#.#...O..OOO.O.#.#......O..#O.#..##.##..#.........O...#OO...#..O.....#O...O..##O#O........
-..O#O.##O.O.#.....#.OO..O.....OOOO#O....O..O.##O#..O..#..O#....##OO#..O...O..##..#O.##.........O#..O
-#.#.#..##..O.O#O...##.#.O....#..#.O#..#....O.....#.#.....#.O.#O.....O.#.##...#..O.O.OOO..O...O......
-....#....O..OO.#...O.O#.#.##..O....O..OO.#....O...O...O.....#......#..OO#OO.OO#..#.O#....#.#.OO..##O
-O.##O.O.O....O..#......O..O.#O#O....#O....#.#.#.#O.........OO..O....O.#......O##OO......OOO..#O..#..
-#.O.O.OO.O..#.....#O..#......O.......#....O.....O.....#...O.#.O......#...###...OO.#..O.OO#.O#..O#.OO
-.....O.#.##OOOOOO#.#OO#O..#...#.........O..O.OO...OO....O..O.......O..O...#.#OO#.O.........O......O.
-O......#..OO.O..O.......#..#...#..O.OO.#..##..#.O...#.O.#.O#O##.O.#.#..#..#....O.....#.#..O.....O#..
-#.....#O...O.O......O......O.O.OO.O#O..#...O..#....#.....#O..O..#O...O..O.OO##.#.##O.#..#..#.#.....O
-O#.O##..###.........O.....O.#.O#....#O.O..O.O#O#....OO..#O..O.....OO.O##..OO.#......#.O......###....
-..O....#...OO.OO#.O#.O.OO#..O...##..OO..O#O#..OO..#.....OO...........O....#...#.......O#....O#......
-....#.#O.O..OO......##.OO.#..##O.....#.......#.........O.O..OO.OO...#.#O.#O...OOOOO..O...OOOO.O#.#..
-....#O##.OO#O..O.O.O#O.#O#..###....OO.....#OO..O.........O.O....OO.O..#..O#..OO...O#O.O.O.......O#..
-O.O#.O.#.OO....OOO.O.O.O...OO.#.O.##..O..O.#..OO.#..O............#O.##.O#.OO.O.##..#....#..OO.O...#.
-.#...O.#.#.#.O.#..##.O##.#..O..O...#.......O...OO.....#...#.OO#..O.OO....O....O.....#O.O#OO.##OO..O.
-O#OO.OO..O..O.......O#.O....OO...O.........OO#.#O#O#.OO....#..#...O...OOO#...O.#O.#...........OO.O#.
-..OO#..O...OO#O..#..#.#...#......O....O#..O.O....O#...##.....O..#O#..........O#.#......#.#.#OOO..O.O
-.OO.......OO.#...O...O#.O...#..OO.##........O.........O..OOO.O..#......O#.OO.O....O.#..OOO....O..O.O
-O#.O.#..O#..#O.#..O.O...##...O#.#O#...#O......O#.....#.###..#....O.....#O#....#....O#.O.O..#OO.O...O
-.O...O...O...O.#.OO....#...O.O..........O...#...#........##..........##...#..O.O.O#O#.#......O..O...
-.#.#..#O.#.....O.#....##........#....#....O.#O.O..............O.O....#..O.O.#O....O#O........#....O.
-O....###O...O..O...OO.....#......O#.O..OO.#.#.O#....#O..#.O..OO..#..O.#.O..#....O...#OO.....O.OO.O..
-#O.O.O.#O...#.OO...........#....O...#O.......#...##..##.##.O.O..........OOO..O..#..OO..O..OO..#.#.#.
-#.O#..#..O.OOOO....OO.#O##.##..#..OO##O#.#.O....O.O#.##.O#...OO.O......#..#...O........O...#.##O..#.
-......#...O......#......O.#..O.OOOO.O.O......#.#O...O#....O..#...........O.O..#...#..#O..O.O..##O..O
-..O.#.....#...O.O..O....O.....#.....#.....O..O.O.O#O.#.....#.#OOOO...O.....O.#..#O..O.OOO....#OOO.O.
-.OOO....O#.........O.......#..O......#..........#...##O...O...O...O.O.O##....O#O##.......##O...#..O.
-#.##.O.O....O......##..#.....##..##..O#....O.#O#.O....#........#.#.#....OO.O.........OO..O......#O#.
-#.O.......#O#.O....O.O.......O.O##O#....#O.O.#O#.O.O.OO..#.O#...O.##.###.O.O#.O..#...#O.....OO....##
-..O......O...#..#.O#....##O......OO......O.O....O....O#...O.O.OOO.#O...O.#..O......OO...#.O#.....#.O
-.OO...#O.O....O.........##......##.......#...O..##.....#...OO.....O...#.#....O..O...O#.......O..O.O.
-.O#.....O....O..O...#.O.O#...#.#..#O#.O.....O#O.OO....#....O..OO.OO.#..#.#.O.O.....O..O......O..#..#
-.O.#...O...O.OOO#O...#........O..#..#.##..#...#...#..O....##.O.O#...O.....O.##.O........O...#O#O#.O.
-.#O....#.O#O##O......O........#...##.#..O.OO..O.....#.#.O#...O.O#.#...#.......#...#.#.O.....O.OOO...
-.O....OO###...O#...O..#..O............#O.#.O.......OO...O...##......O.#..O.O#.#....O..O.#....O...##O
-#..#O#.O...O....OO..O..........OO##.....O......O...##O.#..O..OO#...#.OO..#...#.O..O...O.O#.....#....
-O.O#OO.....O#.O..##.OOO.##....O#...O..#.#...O..OO......O#..O..O..#...#O........##.O##....#O.....OO.O
-......O..O..O....OOO..O...O.O#..#.....O..O..#....#.O##....O.#..##O...O..O..OO....#...O.#..O..OO...#.
-..........O.#O#.#......#......#O..O.O.OOOOO.#...O..#.O.OO....#.O#.#O..#......#.#O.O.O...O..##..O#.O.
-OO#...#...OOO..O.#.......#.O...##..O.O......#..O.......##.....O..O.....OOO.##..O..O.#...#O.......#.#
-....##..#..O......OO.......O#O#O...O.O.#.#..#..##..##O....O.#.........#.O...#..O........O..#..#O.OO.
-......O.O...OO##..O..##.O#O#..O##.O#..#...O##....#.......#....#OO.O.####O...###O...#.........###O..O
-.#.O#.#O..#..#...#O..O..#..........#.......O..#.O...O.#..#O..O...O..#..O.............#.OO....O#..OO.
-.....#.O.OO...#...O...O..........#......#..O.O....OO.#..#...O#O..#.....OO........O.#O.#.......OO#.OO
-OO...#.O.......#O.O...O#.O....O.....OO.O.........##OO...OO...O.#..OO...O.#.O...O.O..O#OO.O#O..#..OO.
-..O##.#..#...O..O.#....O.O##.O.#.O...O..#O.OO...O.#.....##O...O.O.......##O...OO..O#..O#.#......O.O.
-.....O.....OO..O..O..O.#...O......O..OO..OO....OOO#.O..OO......#.O.....#O.O.O#....##OO..#O#.O..O#O.O
-......O.O#...O..O.OO.O.OO.OOO...#O..O#...OO.#O.OO...O..#..O..O.#....O..OO....#......O.O.#O.##..O..O.
-#.....#....O#O..O#O....O.......#.....O....#..#.OOOO#OOO#..#.OO....OO.O#.O......#..O.OO..O.#....O.O..
-.O.O.#.#.O...#..#O.......##...OO..O...#......#.O#...O..O....#O.O.O.........#O.O#..O....##.O.O.......
-....#..O.#.......#.#O..O..........#...#..O##..O#..O..OO.O###.#O#..O#O#O..#OO.#..O#.#.O..#....O.O....
-...##...........O...#O.OOO..##O.O...#.OO.OO..O...O.....#.......#.....#........#....##.....O...#.#..O
-.O......O.#...##O..#O..O......O#O....#.......O....#...O#....O.....#......#O..#.O..#.#.O.O#...O#.....
-O..O.#........O.##.#..##....O.OO.......O.......O..#O#O......O...###.........OO...#.#..#........#.#..
-O.........#..O.O...#O......#.O.#OO..O.##..O.#O.#..#..#.#.O.....#...#.....##....#.......O##...O#..O..
-#OO..........O.O...#.OO....#...#.O#..OO#..#O.##.O....#O#..O....O..#...#.#....O.O...#.#O....OO.#.##..
-.#...#O.#O#O###.OO.#..#....#OO.O....O.O#.O...........O#....O.#.O......#.O.#.#O.#..O.OO.....OO...OO.#
-O#.........O....O.#....##.....O.....O.........#O.O#O...O.O#..O.#O....#.#...O....O#....##.......#....
-.....OO#..O##..O.......O.#...#.OO.O.........#..O###...#.O..O#.O......O.....#.O.OO......#O...O#.....O
-.O.O##.###OO..........O.......OOO..O.OO..##...#O..O...OO..#.O.....O.#..#.O.OO##.#.#...###....O...#..
-.O..###...O...#.OOO#.O#.O#...O.#OO.OO...O..#.O......#.O.O.OO...#OO..#...#..O#O...O........##.OO.#...
-.#..#O......#O......O#...#..#O...##O..#.O..O....O.#O..OO..O..#.#....O..O......O.O.#...##..O.O.#O....
-.O.##.#...#O..#...O#.#.O..O#.......O....#.O.O.#.##.....#.......O.##..#.......O.....##.O.#.....#..#O.
-...#.O.O.#O#.#...#...#OO.#O..O....O...O#....#.#.O..O..OO.#.OO..O...#O....O..#.......OO...O.....#.O..
-O..O...O.......O.....##O#.O.#...OOO.O...........#..O..O.O.....O............#...........#...O.O..#.O.
-..O.O.#O...O#.#.#O.OO.....##..O##O.O.O....#OOO...##.O.O.OO........O.#O........O.#.O.#.O.OO....O#.OO.
-#.O.O##.....OO..#OO.O...O.#..#......O...OO....OO.#...O.##.O.OO.#.##..##O.O...#O#...##.....#..O#O.##.
-..#OO..OO.##..#....O##.#O.OO##.O....#....O...O.........O....O#O.##.OOO.#..#......OO#..OO...#..#..#O.
-.....#.#.O...O##....O.#O..O.#..#..O.#O.#O##..O.#..#O.#.O..O.......##......O#.##.O#..OO.....O#OOO.#..
-O....O.O#.#...O.OO.........O...#...#...OO..##..OO..........#O.O.....#.#.OO.#..O#O.#...OO..O##.#.O...
-..#....##..............O..#.#..OO.OOO..#OO.O#O.OO#..#...O.#...O..O...OO......#.O..#....O.OO.O....##.
-O.#O##....#.OO....#.#...#.....O......O......#.#......#OO.....###...#...#..O...O...O.OO.#...#.......#
-....O..#....#....O...#...O....OO.O##..#.O..OO.#.#.#......#O.OO......O##O.#O...#.#.....O....#O..O....
-#OO.#..O......#.....#........#...#.....O...OOO.......OO##O....#......O.O...O##.O.OOO.#........O.#...
-O.OO.......O..OO.O.OO.OO..OO#...##.#.....#...O.#.O....O...#.OO.##.#.....OO..O#......#OOOO....O....##
-...#...#..#....#.....O..O#O..#.##O.O......O.#O......O..##...#O#O..##..O.#....O.#...O.OO...#O#....OOO
-.........OOOO.O.O.O...#........#....OO.#OO#....#O........#.....#O.....##O.....#O..O.O...#...#....OO."""
 
 data = """O....#....
 O.OO#....#
@@ -243,7 +153,7 @@ O.#..O.#.#
 #OO..#...."""
 
 
-# data = get_data(day=14, year=2023)
+data = get_data(day=14, year=2023)
 
-# part_1(data)
+part_1(data)
 part_2(data)
